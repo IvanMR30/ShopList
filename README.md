98d57834e516dc877a7e48d11b05b1ab67367710
Ivan Alberto Morales Rosas <24470026@campeche.tecnm.mx>
2025-11-19 14:16:27 -0600

    Muestra solo la lista de compras

---
+++ README.md
@@ -0,0 +1,1 @@
+Aplicacion para una lista de compras
---
+++ ShopList.Gui/Views/ShopListPage.xaml.cs
@@ -0,0 +1,13 @@
+using ShopList.Gui.ViewsModels;
+
+namespace ShopList.Gui.Views;
+
+public partial class ShopListPage : ContentPage
+{
+	public ShopListPage()
+	{
+		InitializeComponent();
+		BindingContext = new ShopListViewModel();
+	}
+
+}
